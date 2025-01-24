85ffe7bf4fbab45e22cf2700b8f7d57fb97fe5ff
= <138523300+iamwilllll@users.noreply.github.com>
2025-01-24 19:40:40 -0400

    build: add gulp-terser and and sharp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,57 @@
-import { src, dest, watch, series } from 'gulp'
-import * as dartSass from 'sass'
-import gulpSass from 'gulp-sass'
+import path from 'path';
+import fs from 'fs';
+import { src, dest, watch, series } from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 
-const sass = gulpSass(dartSass)
+const sass = gulpSass(dartSass);
 
-export function js( done ) {
-    src('src/js/app.js')
-        .pipe( dest('build/js') ) 
+import terser from 'gulp-terser';
+import sharp from 'sharp';
 
-    done()
+export function js(done) {
+    src('src/js/app.js').pipe(terser()).pipe(dest('build/js'));
+    done();
 }
 
-export function css( done ) {
-    src('src/scss/app.scss', {sourcemaps: true})
-        .pipe( sass().on('error', sass.logError) )
-        .pipe( dest('build/css', {sourcemaps: '.'}) )
+export function css(done) {
+    src('src/scss/app.scss', { sourcemaps: true })
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('build/css', { sourcemaps: '.' }));
+    done();
+}
 
-    done()
+export async function crop(done) {
+    const inputFolder = 'src/img/gallery/full';
+    const outputFolder = 'src/img/gallery/thumb';
+    const width = 250;
+    const height = 180;
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdirSync(outputFolder, { recursive: true });
+    }
+    const images = fs.readdirSync(inputFolder).filter(file => {
+        return /\.(jpg)$/i.test(path.extname(file));
+    });
+    try {
+        images.forEach(file => {
+            const inputFile = path.join(inputFolder, file);
+            const outputFile = path.join(outputFolder, file);
+            sharp(inputFile)
+                .resize(width, height, {
+                    position: 'centre',
+                })
+                .toFile(outputFile);
+        });
+        done();
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export function dev() {
-    watch('src/scss/**/*.scss', css)
-    watch('src/js/**/*.js', js)
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', js);
 }
 
-export default series( js, css, dev )
+export default series(crop, js, css, dev);
