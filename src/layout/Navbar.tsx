2e95b9bc03c4f908020f383e4d2a47a0f6d7bb32
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-08-31 13:39:20 -0400

    feat: Enhance Navbar styling and functionality

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -12,18 +12,17 @@ function Navbar() {
     const [isActive, setIsActive] = useState(false);
 
     return (
-        <section className="flex justify-end">
+        <>
             <button
                 onClick={() => setIsActive(true)}
-                className="text-first-font-color hover:cursor-pointer md:hidden"
+                className="text-second-font-color hover:cursor-pointer md:hidden fixed z-1 top-5 right-5"
                 aria-label="open navigation menu"
                 title="Open menu"
             >
                 <MenuIcon />
             </button>
-
             <nav
-                className={`absolute transition-all duration-500 ${isActive ? 'h-full top-0' : 'h-0 -top-50'} left-0 w-full md:top-5 md:left-1/2 md:-translate-x-1/2 md:w-8/10 md:h-15 lg:w-5/10 bg-[#ffffff20] flex flex-col md:flex-row items-center justify-center text-first-font-color gap-10 md:rounded-full lg:px-10`}
+                className={`fixed transition-all duration-500 z-1 ${isActive ? 'h-screen top-0' : 'h-0 -top-50'} left-0 w-full md:top-5 md:left-1/2 md:-translate-x-1/2 md:w-9/10 max-w-[768px] md:h-15 lg:w-5/10 bg-[#000000db] md:bg-nav-bg-color flex flex-col md:flex-row items-center justify-center text-second-font-color gap-10 md:rounded-full lg:px-10`}
             >
                 <button
                     aria-label="close navigation menu"
@@ -35,31 +34,31 @@ function Navbar() {
                 </button>
                 <a
                     title="Home"
-                    className="hover:text-second-font-color transition hover:underline text-2xl hover:scale-110 md:text-lg hover:cursor-pointer"
+                    className="hover:text-first-font-color transition hover:underline text-2xl hover:scale-110 md:text-lg hover:cursor-pointer"
                 >
                     HOME
                 </a>
                 <a
                     title="Projects"
-                    className="hover:text-second-font-color transition hover:underline text-2xl hover:scale-110 md:text-lg hover:cursor-pointer"
+                    className="hover:text-first-font-color transition hover:underline text-2xl hover:scale-110 md:text-lg hover:cursor-pointer"
                 >
                     PROJECTS
                 </a>
                 <a
                     title="About me"
-                    className="hover:text-second-font-color transition hover:underline text-2xl hover:scale-110 md:text-lg hover:cursor-pointer"
+                    className="hover:text-first-font-color transition hover:underline text-2xl hover:scale-110 md:text-lg hover:cursor-pointer"
                 >
                     ABOUT ME
                 </a>
                 <a
                     title="Contact me"
-                    className="hover:text-second-font-color transition hover:underline text-2xl hover:scale-110 md:text-lg hover:cursor-pointer"
+                    className="hover:text-first-font-color transition hover:underline text-2xl hover:scale-110 md:text-lg hover:cursor-pointer"
                 >
                     CONTACT ME
                 </a>
                 <SocialLinks />
             </nav>
-        </section>
+        </>
     );
 }
 
