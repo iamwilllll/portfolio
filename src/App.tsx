947c8bc093ab974117ce80c896a97e504d3007d5
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-08-31 13:39:19 -0400

    Refactor: App layout to include Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 /* Import components */
 import Navbar from './layout/Navbar';
+import Header from './layout/Header';
 
 function App() {
     return (
-        <main className="p-5 font-primary">
+        <main className=" max-w-[2256px] font-primary m-auto">
             <Navbar />
+            <Header />
         </main>
     );
 }
