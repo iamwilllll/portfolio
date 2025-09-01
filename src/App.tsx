1e34d068e196fc50cf83a429b9090bd230f6dbe3
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-09-01 18:18:44 -0400

    feat: Add initial Projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 /* Import components */
 import Navbar from './layout/Navbar';
 import Header from './layout/Header';
+import Projects from './layout/Projects';
 
 function App() {
     return (
-        <main className=" max-w-[2256px] font-primary m-auto">
+        <main className=" max-w-[2256px] font-first-font m-auto">
             <Navbar />
             <Header />
+            <Projects/>
         </main>
     );
 }
