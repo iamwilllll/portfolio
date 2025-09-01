c73850f0dec75082d0d80371dd20e2e1c0c91b55
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-09-01 18:18:43 -0400

    style: Rework Header component design

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -11,21 +11,21 @@ function Header() {
                 />
             </figure>
 
-            <div className="flex flex-col items-center lg:items-start gap-5 lg:w-1/2">
+            <div className="flex flex-col items-center lg:items-start gap-10 lg:w-1/2">
                 <aside className="text-center">
                     <h1 className="text-5xl font-semibold text-first-font-color lg:text-left">HI, i'm Wilfryn</h1>
-                    <p className="text-first-font-color text-xl mt-5 font-light lg:text-left">
-                        As an learning <span className="font-bold">Front-End Developer</span>, I combine technical skills with
-                        creativity to build functional and user-friendly applications. Beyond coding, I focus on clear
-                        communication and continuous learning to bring innovative ideas to life.
+                    <p className="text-third-font-color text-xl mt-5 font-light lg:text-left">
+                        As an learning <span className="font-bold text-first-font-color">Front-End Developer</span>, I combine
+                        technical skills with creativity to build functional and user-friendly applications. Beyond coding, I
+                        focus on clear communication and continuous learning to bring innovative ideas to life.
                     </p>
                 </aside>
 
-                <aside className="flex flex-col gap-2 md:flex-row">
+                <aside className="flex flex-col gap-2 lg:gap-5 md:flex-row">
                     <a
                         href="https://docs.google.com/document/d/1m6El1D5_eSy4y-9MZOJ49R9_RGsTCesycKU_e96cg2A/edit?usp=sharing"
                         target="_blank"
-                        className="rounded-full bg-primary-color text-first-font-color hover:bg-white hover:text-second-font-color transition hover:cursor-pointer flex items-center justify-center text-center w-80 h-15 lg:w-50 "
+                        className="flex h-12 items-center align-center gap-2 bg-white px-15 py-2 rounded-full font-bold text-black font-second-font hover:cursor-pointer hover:bg-primary-color hover:text-first-font-color hover:scale-110 transition"
                     >
                         Download CV
                     </a>
@@ -33,7 +33,7 @@ function Header() {
                     <a
                         href="https://www.linkedin.com/in/iamwilllll"
                         target="_blank"
-                        className="rounded-full bg-primary-color text-first-font-color hover:bg-white hover:text-second-font-color transition hover:cursor-pointer flex items-center justify-center text-center w-80 h-15 gap-2 lg:w-50 "
+                        className="flex h-12 items-center align-center gap-2 bg-white px-15 py-2 rounded-full font-bold text-black font-second-font hover:cursor-pointer hover:bg-primary-color hover:text-first-font-color hover:scale-110 transition"
                     >
                         <LinkedinIcon />
                         Linkedin
