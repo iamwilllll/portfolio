d35fcf89cc971c7824eea0623e6fa4589dab2085
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-09-02 13:33:13 -0400

    refactor(projects): Simplify Projects component using Container

--- src/layout/Projects.tsx
+++ src/layout/Projects.tsx
@@ -1,21 +1,6 @@
+import Container from '../components/Container';
 function Projects() {
-    return (
-        <section className="h-screen w-full flex flex-col items-center justify-between p-10 lg:p lg:px-50 lg:items-start overflow-hidden gap-5">
-            <h2 className="text-first-font-color text-4xl lg:text-5xl mt-25">
-                Some of the projects I've done
-            </h2>
-
-            <div className="w-full h-5/10 overflow-x-auto overflow-y-hidden relative">
-                <div className="flex gap-5 w-max">
-                    <div className="w-70 h-50 lg:w-85 lg:h-55 bg-primary-color rounded-4xl flex-shrink-0"></div>
-                    <div className="w-70 h-50 lg:w-85 lg:h-55 bg-primary-color rounded-4xl flex-shrink-0"></div>
-                    <div className="w-70 h-50 lg:w-85 lg:h-55 bg-primary-color rounded-4xl flex-shrink-0"></div>
-                    <div className="w-70 h-50 lg:w-85 lg:h-55 bg-primary-color rounded-4xl flex-shrink-0"></div>
-                    <div className="w-70 h-50 lg:w-85 lg:h-55 bg-primary-color rounded-4xl flex-shrink-0"></div>
-                </div>
-            </div>
-        </section>
-    );
+    return <Container>Projects</Container>;
 }
 
 export default Projects;
