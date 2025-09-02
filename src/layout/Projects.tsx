21f23acc456ed847888db8f9ab0a0047b077fb65
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-09-02 17:00:06 -0400

    feat: Add initial structure for Projects section

--- src/layout/Projects.tsx
+++ src/layout/Projects.tsx
@@ -1,6 +1,21 @@
 import Container from '../components/Container';
+
 function Projects() {
-    return <Container>Projects</Container>;
+    return (
+        <Container className="pt-25">
+            <h2 className="text-first-font-color text-4xl">Some of the projects i've done i did</h2>
+            <section className="flex flex-col gap-10 mt-10">
+                <div>
+                    <div className="flex align-center p-5 h-75 w-125 bg-second-bg-color rounded-[60px]"></div>
+                </div>
+
+                <div className="flex">
+                    <div className="h-75 w-125 bg-second-bg-color rounded-[60px]"></div>
+                    <p className="text-first-font-color">wilfryn</p>
+                </div>
+            </section>
+        </Container>
+    );
 }
 
 export default Projects;
