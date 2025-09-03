cff2e9fd9a1ac3dd78e9791d765442ad4e1de50a
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-09-03 18:15:36 -0400

    feat: Implement ProjectItem component in Projects layout

--- src/layout/Projects.tsx
+++ src/layout/Projects.tsx
@@ -1,18 +1,18 @@
 import Container from '../components/Container';
+import ProjectItem from '../components/ProjectItem';
 
 function Projects() {
     return (
         <Container className="pt-25">
             <h2 className="text-first-font-color text-4xl">Some of the projects i've done i did</h2>
-            <section className="flex flex-col gap-10 mt-10">
-                <div>
-                    <div className="flex align-center p-5 h-75 w-125 bg-second-bg-color rounded-[60px]"></div>
-                </div>
-
-                <div className="flex">
-                    <div className="h-75 w-125 bg-second-bg-color rounded-[60px]"></div>
-                    <p className="text-first-font-color">wilfryn</p>
-                </div>
+            <section className="flex flex-col gap-15 mt-10">
+                <ProjectItem
+                    projectTitle="Project title"
+                    projectDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae sequi vitae labore minus facilis ab
+                    obcaecati quidem explicabo neque nihil debitis."
+                    projectImage={'a'}
+                    technologies={['TypeScript', 'React', 'TailwindCSS']}
+                />
             </section>
         </Container>
     );
