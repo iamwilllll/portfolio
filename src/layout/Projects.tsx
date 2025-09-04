8f2db7aa7ce4bfefa1e098329592af5dfb57979e
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-09-04 17:30:30 -0400

    feat(portfolio): Add Taskodoro project to projects section

--- src/layout/Projects.tsx
+++ src/layout/Projects.tsx
@@ -7,12 +7,14 @@ function Projects() {
             <h2 className="text-first-font-color text-4xl">Some of the projects i've done i did</h2>
             <section className="flex flex-col gap-15 mt-10">
                 <ProjectItem
-                    projectTitle="Project title"
-                    projectDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae sequi vitae labore minus facilis ab
-                    obcaecati quidem explicabo neque nihil debitis."
-                    projectImage={'a'}
-                    technologies={['TypeScript', 'React', 'TailwindCSS']}
+                    projectTitle="Taskodoro"
+                    projectDescription="Taskodoro is an interactive to-do list web app that lets you easily add, complete, and delete tasks. It includes a timer based on the Pomodoro Technique to improve time management."
+                    projectImage={'/projectHover/taskodoro.png'}
+                    technologies={['JavaScript', 'React', 'TailwindCSS']}
+                    gitHunLink='https://github.com/iamwilllll/Taskodoro'
+                    previewLink='https://iamwilllll.github.io/Taskodoro'
                 />
+
             </section>
         </Container>
     );
