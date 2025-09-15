874bef7466b3eae3286eeea09513d460e8ff04b2
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-09-15 11:31:34 -0400

    refactor(layout): Abstract action links in Header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,46 +1,47 @@
-import { LinkedinIcon } from '../components/icons/LinkedinIcon';
+import type { ReactNode } from 'react';
+
 import Container from '../components/Container';
 
+import { LinkedinIcon } from '../components/icons/LinkedinIcon';
+
+type linksListType = {
+    href: string;
+    name: string;
+    icon?: ReactNode;
+};
+
 function Header() {
+    const linksList: linksListType[] = [
+        { name: 'CV', href: 'https://docs.google.com/document/d/1m6El1D5_eSy4y-9MZOJ49R9_RGsTCesycKU_e96cg2A/edit?usp=sharing' },
+        { name: 'Linkedin', href: 'https://www.linkedin.com/in/iamwilllll', icon: <LinkedinIcon /> }
+    ];
     return (
         <Container className="flex flex-col lg:flex-row-reverse justify-center items-center">
             <figure className="w-11/12 lg:w-1/2 flex items-center justify-center">
-                <img
-                    src="./avatar.png"
-                    alt="Avatar image"
-                    className="mx-auto drop-shadow-[0px_0px_150px] drop-shadow-primary-color md:w-100 lg:w-full max-w-100"
-                />
+                <img src="./avatar.png" alt="Avatar image" className="mx-auto drop-shadow-[0px_0px_150px] drop-shadow-primary-color md:w-100 lg:w-full max-w-100" />
             </figure>
 
             <div className="flex flex-col items-center lg:items-start gap-15 lg:w-1/2">
                 <aside className="text-center">
-                    <h1 className="text-5xl font-semibold text-first-font-color lg:text-left lg:text-8xl mb-10">
-                        HI, i'm Wilfryn
-                    </h1>
-                    <p className="text-third-font-color text-xl mt-0 font-light lg:text-left lg:text-2xl lg:leading-10">
-                        As an learning <span className="font-bold text-first-font-color">Front-End Developer</span>, I combine
-                        technical skills with creativity to build functional and user-friendly applications. Beyond coding, I
-                        focus on clear communication and continuous learning to bring innovative ideas to life.
+                    <h1 className="text-5xl font-semibold text-first-font-color lg:text-left lg:text-7xl mb-10">HI, i'm Wilfryn</h1>
+                    <p className="text-third-font-color text-xl mt-0 font-light lg:text-left lg:text-xl lg:leading-10">
+                        As an learning <span className="font-bold text-first-font-color">Front-End Developer</span>, I combine technical skills with creativity to build functional and user-friendly
+                        applications. Beyond coding, I focus on clear communication and continuous learning to bring innovative ideas to life.
                     </p>
                 </aside>
 
                 <aside className="flex flex-col gap-2 lg:gap-5 md:flex-row">
-                    <a
-                        href="https://docs.google.com/document/d/1m6El1D5_eSy4y-9MZOJ49R9_RGsTCesycKU_e96cg2A/edit?usp=sharing"
-                        target="_blank"
-                        className="flex items-center justify-center gap-1 w-65 h-15 rounded-full bg-white font-second-font font-semibold text-xl text-black cursor-pointer hover:scale-110 hover:bg-primary-color hover:text-white transition"
-                    >
-                        Download CV
-                    </a>
-
-                    <a
-                        href="https://www.linkedin.com/in/iamwilllll"
-                        target="_blank"
-                        className="flex items-center justify-center gap-1 w-65 h-15 rounded-full bg-white font-second-font font-semibold text-xl text-black cursor-pointer hover:scale-110 hover:bg-primary-color hover:text-white transition"
-                    >
-                        <LinkedinIcon />
-                        Linkedin
-                    </a>
+                    {linksList.map(item => (
+                        <a
+                            href={item.href}
+                            title={item.name}
+                            target="_blank"
+                            className="flex items-center justify-center gap-1 w-55 h-12 rounded-full bg-white font-second-font font-semibold text-xl text-black cursor-pointer hover:scale-110 hover:bg-primary-color hover:text-white transition"
+                        >
+                            {item.icon}
+                            {item.name}
+                        </a>
+                    ))}
                 </aside>
             </div>
         </Container>
