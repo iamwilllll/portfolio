7e93bb257a5e0fb03917cb03468dc2a491f0138c
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-09-20 08:59:48 -0400

    refactor: Use SVG components for technology icons

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -29,20 +29,26 @@ function ProjectItem({ projectTitle, projectDescription, technologies, projectIm
     return (
         <aside className=" flex flex-col gap-5 lg:flex-row lg:justify-start">
             <figure className="h-75 w-full rounded-[30px] overflow-hidden border-[0.1px] border-border-color md:min-w-125 md:max-w-125 md:items-center md:m-auto lg:items-start lg:mx-0">
-                <img src={projectImage} loading="lazy" className="h-15/10 w-15/10 object-cover object-left-top transition-transform duration-500 hover:scale-125" />
+                <img
+                    src={projectImage}
+                    alt={AltProjectImage}
+                    loading="lazy"
+                    className="h-15/10 w-15/10 object-cover object-left-top transition-transform duration-500 hover:scale-125"
+                />
             </figure>
 
             <div className="flex flex-col gap-5 w-full">
                 <h3 className="text-3xl text-first-font-color font-bold md:text-center lg:text-left">{projectTitle}</h3>
                 <ul className="flex flex-wrap gap-3 md:justify-center w-full lg:justify-start">
                     {technologies.map((item, index) => (
-                        <TechnologiesItem key={index} label={item} src={`/projectIcons/${item}.svg`} />
+                        <TechnologiesItem key={index} label={item} item={item} />
                     ))}
                 </ul>
                 <p className="text-xl text-first-font-color md:text-center lg:text-left">{projectDescription}</p>
                 <div className="flex gap-3 mx-auto lg:m-0">
-                    {linksList.map(item => (
+                    {linksList.map((item, index) => (
                         <a
+                            key={index}
                             href={item.href}
                             target="_blank"
                             className="bg-third-bg-color w-35 h-10 rounded-full flex gap-2 items-center justify-center font-bold font-second-font text-first-font-color hover:scale-110 hover:bg-primary-color transition "
--- src/components/TechnologiesItem.tsx
+++ src/components/TechnologiesItem.tsx
@@ -1,12 +1,16 @@
+import icons from './icons';
+
 type TechnologiesItemProps = {
-    src: string;
+    item: string;
     label: string;
 };
 
-function TechnologiesItem({ src, label }: TechnologiesItemProps) {
+function TechnologiesItem({ item, label }: TechnologiesItemProps) {
+    const Icon = icons[item as keyof typeof icons];
+
     return (
         <li className="flex gap-2 h-10 max-w-40 w-full items-center justify-center bg-second-bg-color rounded-full text-white font-semibold font-second-font hover:bg-third-bg-color transition">
-            <img src={src} alt="technologies icon" className="size-6" />
+            <Icon className="size-6" />
             <p className="">{label}</p>
         </li>
     );
