db1cd76e3ac98b223bbd9e2e4b04ad45d6cc9b57
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-09-20 10:05:40 -0400

    feat(build): Add and configure Terser for minification

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss()],
+
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_debugger: true
+            },
+            format: {
+                comments: false
+            }
+        }
+    }
 });
