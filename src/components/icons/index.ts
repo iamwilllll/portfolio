020e2ff03282e9a1006113c88bab14360a291961
iamwillllll <138523300+iamwillllll@users.noreply.github.com>
2025-09-20 18:52:23 -0400

    feat(icons): Add new icons for skills and tools

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,12 +2,28 @@ import { JavaScript } from './JavaScriptIcon';
 import { TypeScript } from './TypeScriptIcon';
 import { React } from './ReactIcon';
 import { TailwindCSS } from './TailwindIcon';
+import { GitHubIcon } from './GitHubIcon';
+import { HTML } from './HTML';
+import { CSS } from './CSS';
+import { Sass } from './Sass';
+import { VSCode } from './VSCode';
+import { Git } from './Git';
+import { Figma } from './Figma';
+import { Prettier } from './Prettier';
 
 const icons = {
-    JavaScript: JavaScript,
-    React: React,
-    TypeScript: TypeScript,
-    Tailwind: TailwindCSS
+    JavaScript,
+    React,
+    TypeScript,
+    Tailwind: TailwindCSS,
+    GitHub: GitHubIcon,
+    HTML,
+    CSS,
+    Sass,
+    VSCode,
+    Git,
+    Figma,
+    Prettier
 };
 
 export default icons;
