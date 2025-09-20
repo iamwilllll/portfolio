72112c747529a9be1d3304fef0252830a88cb80c
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-09-20 10:06:20 -0400

    feat: add icons index and new icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,13 @@
+import { JavaScript } from './JavaScriptIcon';
+import { TypeScript } from './TypeScriptIcon';
+import { React } from './ReactIcon';
+import { TailwindCSS } from './TailwindIcon';
+
+const icons = {
+    JavaScript: JavaScript,
+    React: React,
+    TypeScript: TypeScript,
+    Tailwind: TailwindCSS
+};
+
+export default icons;
