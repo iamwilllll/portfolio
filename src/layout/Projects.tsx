b8b463b81a6013d1d0badfa3b9af4507e355ecd9
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-10-11 23:28:23 -0400

    Implement code changes to enhance functionality and improve performance

--- src/layout/Projects.tsx
+++ src/layout/Projects.tsx
@@ -7,13 +7,13 @@ function Projects() {
             <h2 className="text-first-font-color text-4xl">Some of the projects i've done i did</h2>
             <section className="mt-10 flex flex-col gap-15">
                 <ProjectItem
-                    projectTitle="Taskodoro"
-                    projectDescription="Taskodoro is an interactive to-do list web app that lets you easily add, complete, and delete tasks. It includes a timer based on the Pomodoro Technique to improve time management."
-                    projectImage={'/taskodoro.png'}
+                    projectTitle="TaskPodoro"
+                    projectDescription="TaskPodoro is an interactive to-do list web app that lets you easily add, complete, and delete tasks. It includes a timer based on the Pomodoro Technique to improve time management."
+                    projectImage={'/taskpodoro.png'}
                     AltProjectImage="Hover project image"
                     technologies={['JavaScript', 'React', 'Tailwind']}
-                    gitHunLink="https://github.com/iamwilllll/Taskodoro"
-                    previewLink="https://iamwilllll.github.io/Taskodoro"
+                    gitHunLink="https://github.com/iamwilllll/TaskPodoro"
+                    previewLink="https://iamwilllll.github.io/TaskPodoro"
                 />
             </section>
         </Container>
