c61d0d19192ecffe6455d419897493191f3ed295
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-11-17 22:12:54 -0400

    Refactor: Use image src instead of Icon component in TechnologiesItem component

--- src/components/TechnologiesItem.tsx
+++ src/components/TechnologiesItem.tsx
@@ -1,22 +1,16 @@
-import icons from './icons';
-
 type TechnologiesItemProps = {
-    item: string;
-    label?: string;
     className?: string;
+    label?: string;
+    src: string;
 };
 
-function TechnologiesItem({ item, label, className }: TechnologiesItemProps) {
-    const Icon = icons[item as keyof typeof icons];
-
-    if (!Icon) return;
-
+function TechnologiesItem({ src, label, className }: TechnologiesItemProps) {
     return (
         <li
             className={`font-second-font flex h-10 items-center justify-center rounded-full font-semibold text-white transition ${className}`}
         >
-            <Icon className="size-6" />
-            <p className="">{label}</p>
+            <img src={src} width={25} alt="technology icon" />
+            <p>{label}</p>
         </li>
     );
 }
