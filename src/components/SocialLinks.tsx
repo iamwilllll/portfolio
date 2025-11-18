95d53f1f6f47ecd46df8fe48ee23ef35590c40d1
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-11-17 22:12:46 -0400

    Feat: Add Doc icon to SocialLinks and adjust styling

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -3,23 +3,36 @@ import type { ReactNode } from 'react';
 
 import { GitHubIcon } from './icons/GitHubIcon';
 import { LinkedinIcon } from './icons/LinkedinIcon';
+import { DocIcon } from './icons/DocIcon';
 
-type socialLinksType = {
+
+
+type SocialLinksProps = {
+    className?: string;
+};
+
+
+type SocialLinksType = {
     icon: ReactNode;
     href: string;
 };
-function SocialLinks() {
-    const socialLinks: socialLinksType[] = [
+
+function SocialLinks({className}:SocialLinksProps) {
+    const socialLinks: SocialLinksType[] = [
         { icon: <GitHubIcon />, href: 'https://github.com/iamwilllll' },
         { icon: <LinkedinIcon />, href: 'https://www.linkedin.com/in/iamwilllll' },
+        {
+            icon: <DocIcon />,
+            href: 'https://docs.google.com/document/d/1m6El1D5_eSy4y-9MZOJ49R9_RGsTCesycKU_e96cg2A/edit?tab=t.0',
+        },
     ];
 
     return (
-        <nav className="flex gap-3">
+        <nav className="flex items-center gap-3">
             {socialLinks.map((item, index) => (
                 <a
                     key={index}
-                    className="text-second-color hover:text-first-font-color transition hover:scale-125 hover:cursor-pointer"
+                    className={`text-second-color hover:text-first-font-color transition hover:scale-125 hover:cursor-pointer ${className}`}
                     title="icon"
                     href={item.href}
                     target="_blank"
