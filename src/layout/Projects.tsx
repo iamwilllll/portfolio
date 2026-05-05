060f2e57169908a340f05294ac8ec6b59aafd3f4
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-05-05 17:32:56 -0400

    Refactor: Update import statements and improve text color consistency in Projects and Technologies components

--- src/layout/Projects.tsx
+++ src/layout/Projects.tsx
@@ -1,12 +1,12 @@
 import { ProjectItem, Container } from '../components';
-import useProjects from '../hooks/useProjects';
+import {useProjects} from '../hooks';
 
 export function Projects() {
     const { projects } = useProjects();
 
     return (
         <Container className="pt-25" id="projects">
-            <h2 className="text-first-font-color text-4xl">Some of the projects i've done i did</h2>
+            <h2 className="text-primary-text text-4xl">Some of the projects i've done i did</h2>
             <section className="mt-10 flex flex-col gap-15">
                 {projects?.map((project, index) => (
                     <ProjectItem key={index} project={project} />
--- src/layout/Technologies.tsx
+++ src/layout/Technologies.tsx
@@ -1,22 +1,22 @@
 import { TechnologiesItem, Container } from '../components';
-import useTechnologies from '../hooks/useTechnologies';
+import { useTechnologies } from '../hooks';
 
 export function Technologies() {
     const { technologies } = useTechnologies();
 
     return (
         <Container id="technologies" className="mt-20 lg:m-auto">
-            <h2 className="text-first-font-color text-4xl">These are the technologies I’ve been using</h2>
+            <h2 className="text-primary-text text-4xl">These are the technologies I’ve been using</h2>
 
             <section className="mt-10 flex flex-col items-center justify-center gap-5 lg:flex-row lg:items-start">
                 {technologies?.map((technology, index) => (
                     <aside
                         key={index}
-                        className="border-border-color flex w-full max-w-100 flex-col flex-wrap gap-2 rounded-xl border p-10"
+                        className="border-default-border flex w-full max-w-100 flex-col flex-wrap gap-2 rounded-xl border p-10"
                     >
                         {technology.technologies.map((item, index) => (
-                            <div key={index} className="text-first-font-color flex items-center gap-2">
-                                <TechnologiesItem className="bg-third-bg-color w-10" key={index} src={item.icon} />
+                            <div key={index} className="text-primary-text flex items-center gap-2">
+                                <TechnologiesItem className="bg-tertiary-surface w-10" key={index} src={item.icon} />
                                 <p>{item.name}</p>
                             </div>
                         ))}
